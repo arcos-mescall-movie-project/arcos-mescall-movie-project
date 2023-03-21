7f506540beb0c74778dcd84e4758e451059483f3
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-21 10:31:38 -0500

    loading added, ajax added, working on pop. movie info

--- js/movies.js
+++ js/movies.js
@@ -1,9 +1,25 @@
 "use strict";
 
 
+
+
+
+// loading message to be replaced with movie info
+let loading = alert("Page loading...");
+
+
+// just in case
+// 'https://glitch.com/edit/#!/better-glowing-algebra?path=db.json%3A1%3A0'
 // pulling data from glitch json file
 $.ajax({
-    url: 'https://glitch.com/edit/#!/better-glowing-algebra?path=db.json%3A1%3A0'
+    url: 'https://better-glowing-algebra.glitch.me/movies'
 }).done(function (data) {
-    // insert stuff
+    console.log(data);
+    let movieList = "";
+    let title = data[0].title;
+    console.log(title);
+    // let directer =;
+    // let rating =;
+    // let genre =;
+    // let id =;
 })
