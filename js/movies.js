0ee3afa7f21c32a5c4e3a763ac5c789b98e8b09a
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-21 10:02:52 -0500

    testing first go

---
+++ js/movies.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+// pulling data from glitch json file
+$.ajax({
+    url: 'https://glitch.com/edit/#!/better-glowing-algebra?path=db.json%3A1%3A0'
+}).done(function (data) {
+    // insert stuff
+})
