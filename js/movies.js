1b8480eea0fde1e4391054d321227a8e6eb42071
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-22 11:20:28 -0500

    working on editing a movie

--- js/movies.js
+++ js/movies.js
@@ -15,24 +15,23 @@ $.ajax({
     let movieList = "";
     let i;
     // for loop to create movie info for empty string
-    for(i = 0; i < data.length; i++) {
+    for (i = 0; i < data.length; i++) {
         let title = data[i].title;
-        let director = data[i].director;
+        // let director = data[i].director;
         let rating = data[i].rating;
-        let genre = data[i].genre;
-        let id = data[i].id;
+        // let genre = data[i].genre;
+        // let id = data[i].id;
         // generating movie info for html
         movieList += '<div class="border border-danger cards">' +
             '<h3>' + title + '</h3>' +
-            '<p>' + director + '</p>' +
+            // '<p>' + director + '</p>' +
             '<p>' + rating + '</p>' +
-            '<p>' + genre + '</p>' +
-            '<p>' + id + '</p>' +
+            // '<p>' + genre + '</p>' +
+            // '<p>' + id + '</p>' +
             '</div>';
-        }
-    // sending movie info to div
+    }
     $('.movies').html(movieList);
-
+})
 
     let movieRating = document.querySelector('.movieRating');
     let movieTitle = document.querySelector('.movieTitle');
@@ -50,7 +49,7 @@ $.ajax({
 // ajax POST request for user adding movie info
         $.ajax({
             type: "POST",
-            url: 'https://better-glowing-algebra.glitch.me/movies',
+            url: 'https://better-glowing-algebra.glitch.me/movies/',
             data: addMovie()
         }).done(function () {
             // document.getElementsByClassName('.movies').innerHTML = addMovie();
@@ -58,28 +57,58 @@ $.ajax({
         console.log("is this working?");
         console.log(data);
     })
-});
 
+//allowing users to edit existing movies
+//     Give users the option to edit an existing movie
+//     A form should be pre-populated with the selected movie's details
+//     Like creating a movie, this should not involve any page reloads, instead your javascript code should make an ajax request when the form is submitted.
 
-// Create a form for adding a new movie that has fields for the movie's title and rating
-// When the form is submitted, the page should not reload / refresh, instead, your javascript should make a POST request to /movies with the information the user put into the form
-// function addMovie(e) {
-//     e.preventDefault(); //<-- why arnt you working
-//     let newMovie = {
-//         title: movieTitle.value,
-//         rating: movieRating.value
-//     };
-//
-// // ajax POST request for user adding movie info
-//     $.ajax({
-//         type: "POST",
-//         url: 'https://better-glowing-algebra.glitch.me/movies',
-//         body: addMovie().send(JSON.stringify(newMovie))
-//     }).done(function (data){
-//         movieTitle.value = "";
-//     })
-// };
+// dropdown for selecting your movie to edit
+$.ajax({
+    url: 'https://better-glowing-algebra.glitch.me/movies'
+}).done(function (data) {
+    console.log(data);
+    // empty string to contain movie info
+    let movieList = "";
+    let i;
+    // for loop to create movie info for empty string
+    for (i = 0; i < data.length; i++) {
+        let title = data[i].title;
+        // let director = data[i].director;
+        let rating = data[i].rating;
+        // let genre = data[i].genre;
+        // let id = data[i].id;
+        // generating movie info for html
+        movieList += '<div class="border border-danger cards">' +
+            '<option><h3>' + title + '</h3>' +
+            " " +
+            '<p>' + rating + '</p></option>' +
+            '</div>';
+    }
+    $('.movieSelection').html(movieList);
 })
+
+// user input for editing movie from selector
+    let movieEdit = document.querySelector('.editMovie');
+    let buttonControlTwo = document.querySelector('.btnTwo');
+
+    buttonControlTwo.addEventListener('click', function (){
+        // calling data so that we may change it
+        $.ajax({
+            type: "PATCH",
+            url: 'https://better-glowing-algebra.glitch.me/movies/'
+        }).done(function (data) {
+            let idSelect = data[i].id;
+            for(let i = 0; i < data.length; i++) {
+                if (data.id === idSelect) {
+                    data.title = movieEdit.value;
+                }
+                ('movies').html(newMovieList);
+            }
+        })
+    })
+
+});
 
 
 
